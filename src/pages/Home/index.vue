9f1fc7b53cdfc07dc4de8f29b69b23e1b3875c5a
shuLai <1811399887@qq.com>
2024-03-08 00:31:48 +0800

    add page

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -11,27 +11,68 @@ onMounted(async () => {
     try {
         let res = await getMenuList();
         fetchData.value = res.data;
-        fetchData.value.forEach((item) => {
-            let query = {
-                key: item.id,
-                label: item.menu_name,
-                title: item.menu_name
-            };
-
-            if (item.levelList) {
-                query.children = item.levelList.map((child) => ({
-                    key: child.id,
-                    label: child.menu_name,
-                    title: child.menu_name
-                }));
-            }
-            menuItems.value.push(query);
-        });
-        console.log(res.data);
     } catch (error) {
         console.log(error);
     }
 });
+const columns = [
+    {
+        title: '菜单名称',
+        width: 100,
+        fixed: 'left',
+        dataIndex: 'menu_name',
+        key: 'menu_name'
+    },
+    {
+        title: '类型',
+        width: 100,
+        dataIndex: 'menu_type',
+        key: 'menu_type'
+    },
+    {
+        title: '排序',
+        width: 100,
+        dataIndex: 'sort',
+        key: 'sort'
+    },
+    {
+        title: '状态',
+        width: 100,
+        dataIndex: 'status',
+        key: 'status    '
+    },
+    {
+        title: 'WebUrl',
+        width: 140,
+        dataIndex: 'web_url',
+        key: 'web_url'
+    },
+    {
+        title: '图标地址',
+        width: 140,
+        dataIndex: 'ico_url',
+        key: 'ico_url'
+    },
+    {
+        title: '小图标样式  ',
+        width: 100,
+        dataIndex: 'ico_class',
+        key: 'ico_class'
+    },
+    {
+        title: '备注',
+        width: 100,
+        dataIndex: 'remark',
+        key: 'remark'
+    },
+    {
+        title: '操作',
+        width: 200,
+        fixed: 'right',
+        dataIndex: 'change',
+        key: 'change'
+    }
+];
 const handleClick = () => {
     console.log(111);
 };
@@ -40,27 +81,60 @@ const handleClick = () => {
 <template>
     <div class="wrap">
         <div class="con-main-wrap">
-            <a-menu
-                style="width: 230px"
-                mode="inline"
-                :items="menuItems"
-                @click="handleClick"
-            ></a-menu>
-            <div class="right-wrap">
-                <div class="add-menu">
-                    <a-button type="primary">添加菜单测试</a-button>
-                </div>
-            </div>
+            <a-button type="primary">添加菜单测试</a-button>
+            <a-table :scroll="{ x: 1240, y: 1000 }" :dataSource="fetchData" :columns="columns">
+                <template #bodyCell="{ column, record }">
+                    <template v-if="column.key === 'menu_name'">
+                        <input v-model="record.menu_name" type="text" />
+                    </template>
+                    <template v-if="column.key === 'menu_type'">
+                        <input v-model="record.menu_type" type="text" />
+                    </template>
+                    <template v-if="column.key === 'sort'">
+                        <input v-model="record.sort" type="text" />
+                    </template>
+                    <template v-if="column.key === 'status'">
+                        <input v-model="record.status" type="text" />
+                    </template>
+                    <template v-if="column.key === 'web_url'">
+                        <input v-model="record.web_url" type="text" />
+                    </template>
+                    <template v-if="column.key === 'ico_url'">
+                        <input v-model="record.ico_url" type="text" />
+                    </template>
+                    <template v-if="column.key === 'ico_class'">
+                        <input v-model="record.ico_class" type="text" />
+                    </template>
+                    <template v-if="column.key === 'remark'">
+                        <input v-model="record.remark" type="text" />
+                    </template>
+                    <template v-if="column.key === 'change'">
+                        <a-button style="margin-right: 10px" type="primary">添加下级</a-button>
+                        <a-button type="primary">编辑</a-button>
+                    </template>
+                </template>
+            </a-table>
         </div>
     </div>
 </template>
 
 <style scoped lang="less">
 .con-main-wrap {
-    .flex-row;
-    justify-content: space-between;
-    padding-top: 20px;
-    .right {
+    padding: 30px 0 0;
+    .ant-table-wrapper {
+        margin-top: 30px;
+    }
+    :deep(.ant-table-body) {
+        min-height: 300px;
+        input {
+            width: 100%;
+            padding: 10px 0;
+            border: none;
+            outline: none;
+            border: 1px solid transparent;
+            background: transparent;
+            border-bottom: 1px solid #eee;
+        }
     }
 }
 </style>
